e69a032ff764e0e1861db3da0cb902e06001221b
Luis-Medina12 <luis.medina0312@gmail.com>
2023-04-15 19:40:04 -0700

    added sorting algos page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,6 +13,7 @@ export default function NavBar () {
             <Link to='/About' className="Links"><span>About Me</span></Link>
             <Link to='/TargetBetterPrice' className="Links"><span>Target Better Price</span></Link>
             <Link to='/SaurianTranslator' className="Links"><span>Saurian</span></Link>
+            <Link to='/SortingAlgorithms' className="Links"><span>Sorting Algorithms</span></Link>
         </div>
     )
 }
